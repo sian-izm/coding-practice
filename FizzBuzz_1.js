2cbe557b387326f73955258b1293a39ff22d3c6b
Shian Izumida <shian.izumida@dena.com>
2013-10-11 11:30:49 +0900

    correct relational operator

--- FizzBuzz_1.js
+++ FizzBuzz_1.js
@@ -3,18 +3,18 @@ function FizzBuzz( count ) {
     var OUTPUT_BUZZ = 'Buzz';
     var regexp = /^[0-9]+$/;
     
-    if (regexp.test( count ) == false) {
+    if (regexp.test( count ) === false) {
         alert("整数を入力してください。");
     }
 
     for ( i = 1; i <= count; i++ ) {
-        if ( i % 3 == 0 && i % 5 == 0 ){
+        if ( i % 3 === 0 && i % 5 === 0 ){
             document.write( OUTPUT_FIZZ + OUTPUT_BUZZ + "<br>");
         }
-        else if ( i % 3 == 0 ) {
+        else if ( i % 3 === 0 ) {
             document.write( OUTPUT_FIZZ + "<br>");
         }
-        else if ( i % 5 == 0 ) {
+        else if ( i % 5 === 0 ) {
             document.write( OUTPUT_BUZZ + "<br>");
         }
         else {
