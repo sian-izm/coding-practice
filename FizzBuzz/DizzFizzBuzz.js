8b64e6a8cf56a38aacc4fca6d7034a97883dd9bf
Shian Izumida <shian.izumida@dena.com>
2013-11-01 17:13:23 +0900

    add prototype.constructor

--- FizzBuzz/DizzFizzBuzz.js
+++ FizzBuzz/DizzFizzBuzz.js
@@ -1,24 +1,4 @@
-// QUnitテスト用サンプルオブジェクト FizzBuzz
-// 継承元FizzBuzzクラス----------------------------------------
-
-var ArgumentNullException = function () { };
-
-var FizzBuzz 
-    = (function () {
-           var FizzBuzz = function () { };
-
-           FizzBuzz.prototype.speak = function ( n ) {
-               if ( !n ) throw new ArgumentNullException();
-               if ( n % 3 === 0 && n % 5 === 0 ) return "FizzBuzz";
-               if ( n % 3 === 0 ) return "Fizz";
-               if ( n % 0 === 5 ) return "Buzz";
-               return n.toString();
-           };
-
-           return FizzBuzz;
-       })();
-
-//----------------------------------------
+var FizzBuzz = require('./FizzBuzz').FizzBuzz;
 
 // define the DizzFizzBuzz class
 function DizzFizzBuzz() {
@@ -28,6 +8,7 @@ function DizzFizzBuzz() {
 
 // inherit FizzBuzz
 DizzFizzBuzz.prototype = new FizzBuzz();
+DizzFizzBuzz.prototype.constructor = DizzFizzBuzz;
 
 // add judge_dizz method
 DizzFizzBuzz.prototype.judge_dizz = function ( n ) {
@@ -40,11 +21,16 @@ DizzFizzBuzz.prototype.judge_dizz = function ( n ) {
     // 7で割り切れるときはDizz
     if ( n % 7 === 0 )  return "Dizz";
     // 上記いずれでも無い時は、0を返す.
-    return 0;
+    FizzBuzz.prototype.speak( n );
 };
 
-var DFB_output = new DizzFizzBuzz();
+var dfbOutput = new DizzFizzBuzz();
 var COUNT = 105;
+
+// output
+for ( var i = 1; i <= COUNT; i++ ){
+    console.log(dfbOutput.speak(i));
+}
 
 // assert関数を使った継承の確認テスト----------------------------------------
 function assert( message, expr ) {
@@ -66,14 +52,3 @@ catch (e) {
     console.log("Test faild: " + e.message);    
 }
 //----------------------------------------
-
-// output
-for ( var i = 1; i <= COUNT; i++ ){
-    var DFB_speak = DFB_output.judge_dizz( i );
-    if ( DFB_speak ) {
-        console.log( DFB_speak );
-    }
-    else {
-        console.log( DFB_output.speak( i ) );
-    }
-}
